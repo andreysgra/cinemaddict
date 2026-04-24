7d66d5b5426704b31972cdc1da8e726e190712ca
Andrey Grachev <andreysgra@gmail.com>
2026-04-24 12:23:39 +0300

    Создаёт модули компонентов.

---
+++ src/view/footer-statistics-view.js
@@ -0,0 +1,15 @@
+import AbstractView from '../framework/view/abstract-view';
+
+const createFooterStatisticsTemplate = () => (
+  '<p>130 291 movies inside</p>'
+);
+
+export default class FooterStatisticsView extends AbstractView {
+  constructor() {
+    super();
+  }
+
+  get template() {
+    return createFooterStatisticsTemplate();
+  }
+}
