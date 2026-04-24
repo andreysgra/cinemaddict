8b6f13fa2ca91d5b87100736fda403a3727f0319
Andrey Grachev <andreysgra@gmail.com>
2026-04-24 12:31:19 +0300

    Создаёт презентер.

---
+++ src/presenter/film-presenter.js
@@ -0,0 +1,38 @@
+import SortView from '../view/sort-view';
+import {render} from '../framework/render';
+import FilmsView from '../view/films-view';
+import FilmsListView from '../view/films-list-view';
+import FilmsListContainerView from '../view/films-list-container-view';
+import {FILMS_COUNT_PER_STEP} from '../const';
+import FilmCardView from '../view/film-card-view';
+import ShowMoreButtonView from '../view/show-more-button-view';
+import FilmDetailsView from '../view/film-details-view';
+
+export default class FilmsPresenter {
+  #container = null;
+  #sortComponent = new SortView();
+  #filmsComponent = new FilmsView();
+  #filmsListComponent = new FilmsListView();
+  #filmsListContainerComponent = new FilmsListContainerView();
+  #showMoreButtonComponent = new ShowMoreButtonView();
+  #filmDetailsComponent = new FilmDetailsView();
+
+  constructor(container) {
+    this.#container = container;
+  }
+
+  init() {
+    render(this.#sortComponent, this.#container);
+    render(this.#filmsComponent, this.#container);
+    render(this.#filmsListComponent, this.#filmsComponent.element);
+    render(this.#filmsListContainerComponent, this.#filmsListComponent.element);
+
+    for (let i = 0; i < FILMS_COUNT_PER_STEP; i++) {
+      render(new FilmCardView(), this.#filmsListContainerComponent.element);
+    }
+
+    render(this.#showMoreButtonComponent, this.#filmsListComponent.element);
+    render(this.#filmDetailsComponent, this.#container.parentElement);
+    this.#filmDetailsComponent.element.style.display = 'none';
+  }
+}
