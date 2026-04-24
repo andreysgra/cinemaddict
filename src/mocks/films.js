5bb98d8ae28d0c2c33fbd9088ebcabdb86bda828
Andrey Grachev <andreysgra@gmail.com>
2026-04-25 00:19:54 +0300

    Создаёт тестовые данные.

---
+++ src/mocks/films.js
@@ -0,0 +1,304 @@
+export const films = [
+  {
+    'id': '7fabbd90-2ce3-4450-b15c-c37f6626f374',
+    'comments': [
+      'ddd7ca68-a4a1-47cd-a27b-ab617992cd36'
+    ],
+    'filmInfo': {
+      'title': 'A Shark Who Stole Us',
+      'alternativeTitle': 'Pioneers Without The Room',
+      'totalRating': 4.5,
+      'poster': 'images/posters/the-dance-of-life.jpg',
+      'ageRating': 0,
+      'director': 'Sergey Ivanov',
+      'writers': [
+        'Sergey Makoveev'
+      ],
+      'actors': [
+        'A Romanov'
+      ],
+      'release': {
+        'date': '2025-04-23T21:00:00.181Z',
+        'releaseCountry': 'Finland'
+      },
+      'duration': 127,
+      'genre': [
+        'Sci-Fi'
+      ],
+      'description': 'A war drama about two young people'
+    },
+    'userDetails': {
+      'watchlist': false,
+      'favorite': false,
+      'alreadyWatched': true,
+      'watchingDate': '2026-04-03T21:00:00.181Z'
+    }
+  },
+  {
+    'id': '587ab56b-13ae-45a4-a4ba-d05c85c88f33',
+    'comments': [
+      '3d741473-a848-405d-8b88-90f4ab649595'
+    ],
+    'filmInfo': {
+      'title': 'Country On The Darkness',
+      'alternative_title': 'Guest In The Void',
+      'totalRating': 7.9,
+      'poster': 'images/posters/made-for-each-other.png',
+      'ageRating': 21,
+      'director': 'Fedor Ivanov',
+      'writers': [
+        'Tim O\'Mara',
+        'Ilya O\'Reilly'
+      ],
+      'actors': [
+        'Marina Makoveev'
+      ],
+      'release': {
+        'date': '2004-04-23T20:00:00.181Z',
+        'releaseCountry': 'Italy'
+      },
+      'duration': 201,
+      'genre': [
+        'Horror',
+        'Comedy',
+        'Family'
+      ],
+      'description': 'Oscar-winning film'
+    },
+    'userDetails': {
+      'watchlist': true,
+      'favorite': false,
+      'alreadyWatched': true,
+      'watchingDate': '2026-04-19T21:00:00.181Z'
+    }
+  },
+  {
+    'id': 'ae7f3fc0-9aba-4078-a4b3-ee87c339a182',
+    'comments': [
+      '90b84d95-1679-431a-8056-9ff9093cd8d2',
+      '4cfaace3-07d9-44f6-a0fb-aae93561e08b',
+      '9c621a20-53d5-4aea-8227-7eecaca9911d'
+    ],
+    'filmInfo': {
+      'title': 'A Lion With Him',
+      'alternativeTitle': 'Pioneers In The Darkness',
+      'totalRating': 4.9,
+      'poster': 'images/posters/the-man-with-the-golden-arm.jpg',
+      'ageRating': 0,
+      'director': 'Alice O\'Reilly',
+      'writers': [
+        'Tim Walker',
+        'Nevada Walker',
+        'Ivan Walker'
+      ],
+      'actors': [
+        'Nevada Romanov'
+      ],
+      'release': {
+        'date': '2023-04-23T21:00:00.181Z',
+        'releaseCountry': 'Finland'
+      },
+      'duration': 135,
+      'genre': [
+        'Animation',
+        'Horror'
+      ],
+      'description': 'A war drama about two young people'
+    },
+    'userDetails': {
+      'watchlist': true,
+      'favorite': false,
+      'alreadyWatched': true,
+      'watchingDate': '2026-04-15T21:00:00.181Z'
+    }
+  },
+  {
+    'id': '5c46ca12-9d76-424c-b812-d690b4f200a1',
+    'comments': [
+      '0eb6d544-d032-4ac5-97c4-194bb45a275a',
+      '7f18cc56-8552-406a-b17c-53c30b3ef379'
+    ],
+    'filmInfo': {
+      'title': 'Laziness Who Sold Himself',
+      'alternativeTitle': 'Friends Who Bought The Room',
+      'totalRating': 4.4,
+      'poster': 'images/posters/popeye-meets-sinbad.png',
+      'ageRating': 6,
+      'director': 'Alice Romanov',
+      'writers': [
+        'Dakota Romanov'
+      ],
+      'actors': [
+        'Sergey Ivanov'
+      ],
+      'release': {
+        'date': '2020-04-23T21:00:00.181Z',
+        'releaseCountry': 'Spain'
+      },
+      'duration': 72,
+      'genre': [
+        'Drama'
+      ],
+      'description': 'From the creators of timeless classic "Nu, Pogodi!" and "Alice in Wonderland"'
+    },
+    'userDetails': {
+      'watchlist': false,
+      'favorite': false,
+      'alreadyWatched': true,
+      'watchingDate': '2026-04-12T21:00:00.181Z'
+    }
+  },
+  {
+    'id': '76651420-8f0d-4e0f-86bf-e668258d96de',
+    'comments': [
+      '505108d0-2cff-4a22-800a-da6f54a3e6d1'
+    ],
+    'filmInfo': {
+      'title': 'A Little Pony Who Sold Himself',
+      'alternativeTitle': 'Raiders Who Bought The Floor',
+      'totalRating': 6.7,
+      'poster': 'images/posters/the-great-flamarion.jpg',
+      'ageRating': 6,
+      'director': 'Sergey Ivanov',
+      'writers': [
+        'Fedor O\'Mara'
+      ],
+      'actors': [
+        'Fedor Romanov',
+        'Nevada Lee',
+        'Fedor O\'Reilly'
+      ],
+      'release': {
+        'date': '2001-04-23T20:00:00.182Z',
+        'releaseCountry': 'France'
+      },
+      'duration': 200,
+      'genre': [
+        'Thriller'
+      ],
+      'description': 'Oscar-winning film'
+    },
+    'userDetails': {
+      'watchlist': true,
+      'favorite': false,
+      'alreadyWatched': true,
+      'watchingDate': '2026-04-10T21:00:00.182Z'
+    }
+  },
+  {
+    'id': 'dd45cbf3-bbaa-4c93-8d19-acc66f176f72',
+    'comments': [
+      '801f0f56-ab24-476f-ab5b-202bb65fdb86',
+      'c6b0ac7e-6d06-4472-8210-83766b1f6006'
+    ],
+    'filmInfo': {
+      'title': 'Pioneers In The Floor',
+      'alternativeTitle': 'A Little Pony Who Saw The Carpet',
+      'totalRating': 7.2,
+      'poster': 'images/posters/the-man-with-the-golden-arm.jpg',
+      'ageRating': 6,
+      'director': 'Nevada Makoveev',
+      'writers': [
+        'Alice Walker',
+        'Nevada Walker',
+        'A O\'Reilly'
+      ],
+      'actors': [
+        'Tim O\'Mara'
+      ],
+      'release': {
+        'date': '2011-04-23T20:00:00.182Z',
+        'releaseCountry': 'Japan'
+      },
+      'duration': 103,
+      'genre': [
+        'Drama'
+      ],
+      'description': 'Oscar-winning film'
+    },
+    'userDetails': {
+      'watchlist': false,
+      'favorite': false,
+      'alreadyWatched': true,
+      'watchingDate': '2026-04-06T21:00:00.182Z'
+    }
+  },
+  {
+    'id': '279eca74-63dd-4ee8-924d-b02f26de78cf',
+    'comments': [
+      'e1d7d664-3943-4c49-984b-0a6a5ce0a2da',
+      'c2ed8acd-4293-43a3-b06a-57e5abab08f4'
+    ],
+    'filmInfo': {
+      'title': 'Happiness Within Himself',
+      'alternativeTitle': 'Happiness Who Bought Themselves',
+      'totalRating': 6.9,
+      'poster': 'images/posters/the-dance-of-life.jpg',
+      'ageRating': 18,
+      'director': 'Natasha O\'Caml',
+      'writers': [
+        'Marina Lee',
+        'Marina O\'Reilly',
+        'A Walker'
+      ],
+      'actors': [
+        'Natasha Lee',
+        'Marina Makoveev',
+        'Fedor O\'Caml'
+      ],
+      'release': {
+        'date': '2013-04-23T20:00:00.182Z',
+        'releaseCountry': 'Germany'
+      },
+      'duration': 107,
+      'genre': [
+        'Thriller',
+        'Horror',
+        'Family'
+      ],
+      'description': 'True masterpiece where love and death are closer to heroes than their family'
+    },
+    'userDetails': {
+      'watchlist': true,
+      'favorite': false,
+      'alreadyWatched': true,
+      'watchingDate': '2026-04-11T21:00:00.182Z'
+    }
+  },
+  {
+    'id': '343e6a20-112f-436c-9d93-9b2ae1a14bcb',
+    'comments': [
+      'bf8def73-255a-4ff1-854a-e8694ec1fbef',
+      '24f1d0c1-f093-4ebe-a942-675788413509'
+    ],
+    'filmInfo': {
+      'title': 'A Tale Of A Little Bird Who The Darkness',
+      'alternativeTitle': 'Happiness Who Bought The Room',
+      'totalRating': 9.4,
+      'poster': 'images/posters/santa-claus-conquers-the-martians.jpg',
+      'ageRating': 18,
+      'director': 'Ilya Makoveev',
+      'writers': [
+        'Fedor O\'Reilly'
+      ],
+      'actors': [
+        'Tim Lee'
+      ],
+      'release': {
+        'date': '2012-04-23T20:00:00.182Z',
+        'releaseCountry': 'Spain'
+      },
+      'duration': 178,
+      'genre': [
+        'Sci-Fi'
+      ],
+      'description': 'Oscar-winning film'
+    },
+    'userDetails': {
+      'watchlist': false,
+      'favorite': false,
+      'alreadyWatched': true,
+      'watchingDate': '2026-03-27T21:00:00.182Z'
+    }
+  },
+];
