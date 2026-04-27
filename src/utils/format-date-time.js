4f666ce7c1d3f4a7dae686908f74594505de85f2
Andrey Grachev <andreysgra@gmail.com>
2026-04-27 18:20:58 +0300

    Реализует отображение данных в представлении окна подробной информации о фильме.

--- src/utils/format-date-time.js
+++ src/utils/format-date-time.js
@@ -1,8 +1,16 @@
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 dayjs.extend(duration);
+dayjs.extend(relativeTime);
 
 export const getFullYear = (date) => dayjs(date).format('YYYY');
 
 export const getDuration = (minutes) => dayjs.duration(minutes, 'minutes').format('H[h] mm[m]');
+
+export const getFullDateMonthAsString = (date) => dayjs(date).format('DD MMMM YYYY');
+
+export const getFullDateWithTime = (date) => dayjs(date).format('YYYY/MM/DD HH:mm');
+
+export const getHumanizeDate = (date) => dayjs.duration(dayjs(date).diff(dayjs())).humanize(true);
