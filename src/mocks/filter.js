b81ae87206da80448e2b14446cb24e9110f1c5b5
Andrey Grachev <andreysgra@gmail.com>
2026-05-09 20:52:37 +0300

    Создаёт структуру данных для фильтров.

---
+++ src/mocks/filter.js
@@ -0,0 +1,9 @@
+import {filter} from '../utils/filter';
+
+export const generateFilters = (films) => Object.entries(filter)
+  .map(([filterName, filterFilms]) => (
+    {
+      name: filterName,
+      count: filterFilms(films).length
+    }
+  ));
