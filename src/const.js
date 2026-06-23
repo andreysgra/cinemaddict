4d176f2e26f1b87d17b7b10aad632c5016d11957
Andrey Grachev <andreysgra@gmail.com>
2026-06-23 11:20:24 +0300

    Добавляет компонент сообщения об отсутствии фильмов в системе.

--- src/const.js
+++ src/const.js
@@ -3,3 +3,17 @@ export const FILMS_COUNT_PER_STEP = 5;
 export const DESCRIPTION_LENGTH = 140;
 
 export const EMOTIONS = ['smile', 'sleeping', 'puke', 'angry'];
+
+export const FilterType = {
+  ALL: 'all',
+  WATCHLIST: 'watchlist',
+  HISTORY: 'history',
+  FAVORITES: 'favorites',
+};
+
+export const NoFilmsMessage = {
+  [FilterType.ALL]: 'There are no movies in our database',
+  [FilterType.WATCHLIST]: 'There are no movies to watch now',
+  [FilterType.HISTORY]: 'There are no watched movies now',
+  [FilterType.FAVORITES]: 'There are no favorite movies now'
+};
