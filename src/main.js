0d3b247d51c1a3fcabf61d5005d8078f478530a1
Andrey Grachev <andreysgra@gmail.com>
2026-06-23 18:01:46 +0300

    Создаёт презентер фильтров.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import {render} from './framework/render';
 import UserProfileView from './view/user-profile-view';
-import FiltersView from './view/filters-view';
 import FooterStatisticsView from './view/footer-statistics-view';
 import FilmsPresenter from './presenter/films-presenter';
 import FilmsModel from './model/films-model';
 import CommentsModel from './model/comments-model';
 import {films} from './mocks/films';
 import {comments} from './mocks/comments';
+import FilterPresenter from './presenter/filter-presenter';
 
 const bodyElement = document.body;
 const siteHeaderElement = bodyElement.querySelector('.header');
@@ -17,8 +17,12 @@ const filmsModel = new FilmsModel(films);
 const commentsModel = new CommentsModel(comments);
 
 const userProfileComponent = new UserProfileView();
-const filtersComponent = new FiltersView();
 const footerStatisticsComponent = new FooterStatisticsView();
+
+const filterPresenter = new FilterPresenter({
+  container: siteMainElement,
+  filmsModel
+});
 
 const filmsPresenter = new FilmsPresenter({
   container: siteMainElement,
@@ -27,7 +31,7 @@ const filmsPresenter = new FilmsPresenter({
 });
 
 render(userProfileComponent, siteHeaderElement);
-render(filtersComponent, siteMainElement);
 render(footerStatisticsComponent, siteFooterStatisticsElement);
 
+filterPresenter.init();
 filmsPresenter.init();
