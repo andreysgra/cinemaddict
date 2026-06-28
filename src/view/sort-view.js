72a6a39bd89d3187c60c90347ab050d205b9e919
Andrey Grachev <andreysgra@gmail.com>
2026-06-28 19:03:09 +0300

    Описывает обработчик события активного типа сортировки.

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,19 +1,46 @@
 import AbstractView from '../framework/view/abstract-view';
+import {SortType} from '../const';
+import classNames from 'classnames';
 
-const createSortTemplate = () => `
-  <ul class="sort">
-    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" class="sort__button">Sort by date</a></li>
-    <li><a href="#" class="sort__button">Sort by rating</a></li>
-  </ul>
+const createSortItemTemplate = (currentSortType, sortType) => `
+    <li>
+      <a href="#"
+        class="${classNames('sort__button', {'sort__button--active': currentSortType === sortType})}"
+        data-sort-type="${sortType}">
+        Sort by ${sortType}
+      </a>
+    </li>
 `;
 
+const createSortTemplate = (currentSortType) => `
+    <ul class="sort">
+      ${Object.values(SortType).map((sortType) => createSortItemTemplate(currentSortType, sortType)).join('')}
+    </ul>
+  `;
+
 export default class SortView extends AbstractView {
-  constructor() {
+  #sortType = null;
+
+  #handleSortTypeChange = () => null;
+
+  constructor({sortType, onSortTypeChange}) {
     super();
+
+    this.#sortType = sortType;
+    this.#handleSortTypeChange = onSortTypeChange;
+
+    this.element.addEventListener('click', this.#sortTypeChangeHandler);
   }
 
   get template() {
-    return createSortTemplate();
+    return createSortTemplate(this.#sortType);
   }
+
+  #sortTypeChangeHandler = (evt) => {
+    if (evt.target.closest('.sort__button')) {
+      evt.preventDefault();
+
+      this.#handleSortTypeChange(evt.target.dataset.sortType);
+    }
+  };
 }
