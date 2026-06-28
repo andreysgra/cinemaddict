ff1628442f0b99308de447178aef7b156a9553bf
Andrey Grachev <andreysgra@gmail.com>
2026-06-28 19:04:54 +0300

    Реализует логику сортировки фильмов.

--- src/utils/film.js
+++ src/utils/film.js
@@ -1,3 +1,8 @@
 import dayjs from 'dayjs';
 
 export const sortCommentsByDate = (commentA, commentB) => dayjs(commentA.date).diff(dayjs(commentB.date));
+
+export const sortFilmsByDate = (filmA, filmB) => dayjs(filmB.filmInfo.release.date)
+  .diff(dayjs(filmA.filmInfo.release.date));
+
+export const sortFilmsByRating = (filmA, filmB) => filmB.filmInfo.totalRating - filmA.filmInfo.totalRating;
